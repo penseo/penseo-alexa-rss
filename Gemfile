a5bbade0725f2db1f45c1d4067ce3f6fe965c1b8
phoet <phoetmail@googlemail.com>
2017-08-07 12:26:17 +0200

    doit

---
+++ Gemfile
@@ -0,0 +1,9 @@
+source 'https://rubygems.org'
+
+ruby '2.4'
+
+gem 'sinatra'
+
+group :development do
+  gem 'foreman'
+end
---
+++ app.rb
@@ -0,0 +1,26 @@
+require 'rss'
+require 'open-uri'
+
+get '/' do
+  content_type 'application/rss+xml'
+  url = 'https://www.penseo.de/post/rss.xml'
+  open(url) do |rss|
+    feed = RSS::Parser.parse(rss)
+    rss = RSS::Maker.make("atom") do |maker|
+      maker.channel.id      = url
+      maker.channel.author  = url
+      maker.channel.pubDate = feed.channel.pubDate
+      maker.channel.title   = feed.channel.title
+
+      feed.items.each do |item|
+        maker.items.new_item do |i|
+          i.link         = item.link
+          i.title        = item.title
+          i.updated      = item.pubDate
+          i.description  = item.description
+        end
+      end
+    end
+    return rss.to_s
+  end
+end
