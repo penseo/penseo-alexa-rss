9b16107af9c33c12d52e0f8b43e4f4b02b81551e
phoet <phoetmail@googlemail.com>
2017-11-06 10:21:57 +0100

    update items to recent date

--- app.rb
+++ app.rb
@@ -1,18 +1,22 @@
+require 'byebug' rescue puts 'no byebug'
 require 'rss'
 require 'open-uri'
 
 get '/' do
   content_type 'application/rss+xml'
   url = params['url'] || 'https://www.penseo.de/post/rss.xml'
+
   open(url) do |rss|
     feed = RSS::Parser.parse(rss)
     rss = RSS::Maker.make("atom") do |maker|
       maker.channel.id      = url
       maker.channel.author  = url
-      maker.channel.pubDate = feed.channel.pubDate
+      maker.channel.pubDate = Time.now
       maker.channel.title   = feed.channel.title
 
-      feed.items.each do |item|
+      items = feed.items.sort_by(&:pubDate).reverse.to_enum.with_index { |item, index| item.pubDate = Time.now - index * 60 * 60 * 24 }
+
+      items.each do |item|
         maker.items.new_item do |i|
           i.link         = item.link
           i.title        = item.title
@@ -21,6 +25,7 @@ get '/' do
         end
       end
     end
-    return rss.to_s
+
+    rss.to_s
   end
 end
